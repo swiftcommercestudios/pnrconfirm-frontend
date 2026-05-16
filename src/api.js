83b4ec8fb17e8e526d910489e29006f7b3329124
SwiftCommerce Studios <swiftcommercestudios@gmail.com>
2026-05-17 01:08:45 +0530

    Switch backend URL to Render

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BACKEND_URL = 'https://web-production-8bac5.up.railway.app'
+const BACKEND_URL = 'https://pnrconfirm-api.onrender.com'
 
 const api = axios.create({
   baseURL: BACKEND_URL,
